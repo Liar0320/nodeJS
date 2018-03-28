deb9df6b73e31b24fd58baebde9d5bed3c5ad3dc
Liar0320 <100108350@qq.com>
2018-03-28 23:33:58 +0800

    serverHtpp

---
+++ serverHttp/server9091.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+ 
+app.use(express.static('public'));
+ 
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+ 
+app.get('/process_get', function (req, res) {
+ 
+   // 输出 JSON 格式
+  //  var response = {
+  //      "first_name":req.query.first_name,
+  //      "last_name":req.query.last_name
+  //  };
+//   console.log(req.query);
+   const vaild = function(name,psw){
+      return name==='liar'&&psw==='0320'
+   }
+   
+   res.end(vaild(req.query.name,req.query.psw));
+})
+ 
+var server = app.listen(9091, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
